2a7b824156784f594583bfc3c75d5d7740689b52
Yash Jha <yjha1@ualberta.ca>
2025-03-11 01:11:06 -0600

    Feat: added the code to authenticate with firebase

--- src/app/adminLogin.tsx
+++ src/app/adminLogin.tsx
@@ -1,26 +1,58 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Inter } from "next/font/google";
 import LogoHeader from "./components/LogoHeader";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "./services/firebase";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function AdminLoginCard() {
+  const [adminId, setAdminId] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setErrorMessage("");
+    setLoading(true);
+    e.preventDefault();
+    try {
+      const q = query(collection(db, "it_admin"), where("hashedIdentificationNumber", "==", adminId));
+      const querySnapshot = await getDocs(q);
+      if (!querySnapshot.empty) {
+        console.log(querySnapshot.docs[0].data());
+      } else {
+        setErrorMessage("Invalid Credentials");
+      }
+    } catch (error) {
+      console.log(error);
+      setErrorMessage("Invalid Credentials");
+    }
+    setLoading(false);
+  };
+
   return (
     <div style={{flex:2}}>
     <div className={`${inter.className} login-card`}>
-      <form action="" className="login-form">
+      <form action="" className="login-form" onSubmit={handleSubmit}>
         <h1 style={{fontSize: 24}}>Admin</h1>
         <div style={{display: "flex", flexDirection: "column", gap: 32, marginTop: 32}}>
           <div>
             <label htmlFor="admin-id">Identification Number</label> <br />
-            <input className="text-box-entry" type="number" id="admin-id" name="admin-id" placeholder="Identification Number" /> <br />
+            <input className="text-box-entry" type="text" id="admin-id" name="admin-id" placeholder="Identification Number" onChange={(e) => setAdminId(e.target.value)} /> <br />
             <label htmlFor="admin-id">Password</label> <br />
             <input className="text-box-entry" type="password" id="password" name="password" placeholder="Password" /> <br />
           </div>
           {/* ask for  forgot password ?*/}
       
           <button type="button" style={{float: "right"}}>Forgot Password?</button>
-          <button type="submit" className="form-submit-button">Sign In</button>
+          {errorMessage? <div className="error-message">
+            <p>{errorMessage}</p>
+          </div> : null}
+          <button type="submit" className="form-submit-button" disabled={loading}>Sign In</button>
         </div>
       </form>
       </div>
