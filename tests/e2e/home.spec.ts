a99171bdaeda75ffb22cbcb18cb86632b4aee439
Richard <richard9@ualberta.ca>
2025-03-17 20:17:07 -0600

    unit test

--- tests/e2e/home.spec.ts
+++ tests/e2e/home.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Home Page has a title', async({page}) => {
-    await page.goto('http://localhost:3000');
-    const title = await page.title();
-    expect(title).not.toBeNull();
+test("Home Page has a title", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+  const title = await page.title();
+  expect(title).not.toBeNull();
 });
